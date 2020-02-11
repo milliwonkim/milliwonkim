fca901110e7816075213f2b5cc9f531fab1fe020
milliwonkim <rldnjs9347@gmail.com>
2020-02-11 10:20:00 +0900

    make it better

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </Provider>
