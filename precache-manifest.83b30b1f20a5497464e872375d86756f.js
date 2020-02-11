ec31d84dfd03acd9e75936364daf280c445b5ad5
milliwonkim <rldnjs9347@gmail.com>
2020-02-11 09:59:52 +0900

    Updates

---
+++ precache-manifest.83b30b1f20a5497464e872375d86756f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f466df4901f621cbf935a5ee49c3409e",
+    "url": "/milliwonkimonaire/index.html"
+  },
+  {
+    "revision": "28f12e5e5b5d3ad234d9",
+    "url": "/milliwonkimonaire/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "cd8e1a1eccfe3ba4e73d",
+    "url": "/milliwonkimonaire/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "28f12e5e5b5d3ad234d9",
+    "url": "/milliwonkimonaire/static/js/2.98a7c26b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/milliwonkimonaire/static/js/2.98a7c26b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8e1a1eccfe3ba4e73d",
+    "url": "/milliwonkimonaire/static/js/main.f56d11a1.chunk.js"
+  },
+  {
+    "revision": "1423a18aa454ae4e0083",
+    "url": "/milliwonkimonaire/static/js/runtime-main.e5836496.js"
+  },
+  {
+    "revision": "648d0b22e9db3abfbff2c7ec2623eafd",
+    "url": "/milliwonkimonaire/static/media/home1.648d0b22.jpg"
+  }
+]);
